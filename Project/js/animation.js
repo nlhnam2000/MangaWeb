5e8feb1a2be32d548708b80d770f86f6168a5c9a
Hoàng  Nam <hoangnam@MacBook-Air-cua-Hoang-2.local>
2020-08-08 00:00:59 +0700

    Thêm hiệu ứng, hoàn thiện trang chi tiết và form đăng nhập, đăng ký

--- Project/js/animation.js
+++ Project/js/animation.js
@@ -7,42 +7,15 @@
 //     history.back();
 // }
 
-function OpenSignInForm() {
-    document.getElementById("SignIn").style.display = "block"; 
-    document.querySelector("nav").style.filter = "brightness(60%)"; 
-    document.getElementById("cover").style.filter = "brightness(60%)"; 
-    document.getElementById("category").style.filter = "brightness(60%)"; 
-    document.querySelector("main").style.filter = "brightness(60%)"; 
-    document.querySelector("div.sidebar").style.filter = "brightness(60%)"; 
-    document.querySelector("footer").style.filter = "brightness(60%)"; 
-}
-
-function CloseSignInForm() {
-    document.getElementById("SignIn").style.display = "none"; 
-    document.querySelector("nav").style.filter = "brightness(100%)"; 
-    document.getElementById("cover").style.filter = "brightness(100%)"; 
-    document.getElementById("category").style.filter = "brightness(100%)"; 
-    document.querySelector("main").style.filter = "brightness(100%)"; 
-    document.querySelector("div.sidebar").style.filter = "brightness(100%)"; 
-    document.querySelector("footer").style.filter = "brightness(100%)";  
-}
-
-function OpenSignUpForm() {
-    document.getElementById("SignUp").style.display = "block"; 
-    document.querySelector("nav").style.filter = "brightness(60%)"; 
-    document.getElementById("cover").style.filter = "brightness(60%)"; 
-    document.getElementById("category").style.filter = "brightness(60%)"; 
-    document.querySelector("main").style.filter = "brightness(60%)"; 
-    document.querySelector("div.sidebar").style.filter = "brightness(60%)"; 
-    document.querySelector("footer").style.filter = "brightness(60%)"; 
-}
-
-function CloseSignUpForm() {
-    document.getElementById("SignUp").style.display = "none"; 
-    document.querySelector("nav").style.filter = "brightness(100%)"; 
-    document.getElementById("cover").style.filter = "brightness(100%)"; 
-    document.getElementById("category").style.filter = "brightness(100%)"; 
-    document.querySelector("main").style.filter = "brightness(100%)"; 
-    document.querySelector("div.sidebar").style.filter = "brightness(100%)"; 
-    document.querySelector("footer").style.filter = "brightness(100%)";  
-}
+function Vote(x) {
+    // kiểm tra button có chứa icon like
+    // nếu có, xoá like, thêm dislike và ngược lại
+    if (x.classList.contains("fa-thumbs-up")) {
+        x.classList.remove("fa-thumbs-up"); 
+        x.classList.add("fa-thumbs-down"); 
+    }
+    else {
+        x.classList.remove("fa-thumbs-down"); 
+        x.classList.add("fa-thumbs-up"); 
+    }
+}       
