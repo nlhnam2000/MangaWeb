9500c7ecc9d9976f8ae58c2255f4625363912d3c
Hoàng  Nam <hoangnam@MacBook-Air-cua-Hoang-2.local>
2020-08-12 10:13:45 +0700

    Thêm mục tất cả thể loại

--- Project/js/animation.js
+++ Project/js/animation.js
@@ -1,12 +1,3 @@
-// function myFunction_Click() {
-//     document.getElementById("doraemon").innerHTML = "Chưa có truyện nhea";
-//     document.getElementById("doraemon").style.background = "red";
-// }
-
-// function quay_lai_trang_truoc() {
-//     history.back();
-// }
-
 function Vote(x) {
     // kiểm tra button có chứa icon like
     // nếu có, xoá like, thêm dislike và ngược lại
@@ -18,4 +9,4 @@ function Vote(x) {
         x.classList.remove("fa-thumbs-down"); 
         x.classList.add("fa-thumbs-up"); 
     }
-}       
+}
