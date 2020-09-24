7b09755023d6466f858ff176e583bdd11a20ffb9
anhuynh2000 <hna.anhuynh2000@gmail.com>
2020-09-24 17:59:24 +0700

    Update server.js

--- Project/server.js
+++ Project/server.js
@@ -41,7 +41,7 @@ require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
