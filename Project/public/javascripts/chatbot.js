8115faa713f6aaef2ffe1b5829da02a570dd0ec9
irisdecembre <thanhtrang181220@gmail.com>
2020-09-24 16:29:07 +0700

    Chinh sua chatbot v1

--- Project/public/javascripts/chatbot.js
+++ Project/public/javascripts/chatbot.js
@@ -120,16 +120,4 @@ function addChat(input, product) {
   botDiv.className = "bot";
   botDiv.innerHTML = ` <span class="bot-response">${product}</span>`;
   chatting.appendChild(botDiv);
-  // speak(product);
-}
-function speak(string) {
-  const u = new SpeechSynthesisUtterance();
-  allVoices = speechSynthesis.getVoices();
-  u.voice = allVoices.filter(voice => voice.name === "Alex")[0];
-  u.text = string;
-  u.lang = "en-US";
-  u.volume = 1; //0-1 interval
-  u.rate = 1;
-  u.pitch = 1; //0-2 interval
-  speechSynthesis.speak(u);
 }
