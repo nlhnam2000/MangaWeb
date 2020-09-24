5662d0bc20b1cf71e32c9d349f3b625346cf1088
anhuynh2000 <hna.anhuynh2000@gmail.com>
2020-09-24 18:35:08 +0700

    Update

--- Project/app/controllers/auth.controller.js
+++ Project/app/controllers/auth.controller.js
@@ -98,9 +98,6 @@ exports.signin = (req, res) => {
       for (let i = 0; i < user.roles.length; i++) {
         authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
       }
-      res.status(200).send({
-        id: user._id,
-        username: user.username,
-      });
+      res.redirect('http://localhost:3000');
     });
 };
